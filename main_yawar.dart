7f3accd32e21964b33457541679338031a797dca
Yawar2002 <83835096+Yawar2002@users.noreply.github.com>
2021-05-10 05:22:04 +0500

    Update main_yawar.dart

--- main_yawar.dart
+++ main_yawar.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Text("Yawar Ali"),
-        ),
-    );
+        ), //Scaffold
+    ); //MaterialApp
   }
 }
